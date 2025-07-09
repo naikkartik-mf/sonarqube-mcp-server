f40ad433ca1e6d43d88366007ee6afcac6780326
naikkartik-mf <naik.kartik@moneyforward.co.jp>
2025-07-09 21:41:46 +0530

    removing dist packages etc

--- dist/config.d.ts
+++
@@ -1,4 +0,0 @@
-import { SonarQubeConfig } from './types.js';
-export declare function loadConfig(): SonarQubeConfig;
-export declare function validateConfig(config: SonarQubeConfig): void;
-//# sourceMappingURL=config.d.ts.map
--- dist/git-utils.d.ts
+++
@@ -1,4 +0,0 @@
-export declare function getCurrentBranch(workingDir?: string): string | null;
-export declare function getRepositoryRoot(): string | null;
-export declare function isGitRepository(workingDir?: string): boolean;
-//# sourceMappingURL=git-utils.d.ts.map
